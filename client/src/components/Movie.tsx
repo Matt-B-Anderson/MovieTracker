96860bec890db670ed45ab20004664d213060ba6
Matthew Anders <manderson0182@gmail.com>
2024-04-16 01:39:55 -0400

    Updated height of body

--- client/src/components/Movie.tsx
+++ client/src/components/Movie.tsx
@@ -10,16 +10,15 @@ const Movie: React.FC<Props> = ({ movie }) => {
     return (
         <Card className='py-3'>
             <Checkbox />
-            <CardHeader className='pb-0 pt-2 px-4 flex-col items-start'>
+            <CardHeader className='pb-0 pt-2 px-4 grid grid-flow-col auto-cols-max'>
                 <p className='text-tiny upercase font-bold'>{movie.title}</p>
                 <small className='text-defualt-500'>{movie.release_date}</small>
             </CardHeader>
             <CardBody className='overflow-visible py-2'>
                 <Image
                     alt='Movie Poster'
-                    className='object-cover rounded-xl'
+                    className='object-cover rounded-xl max-w-60 min-w-60'
                     src={`https://www.themoviedb.org/t/p/original${movie.poster_path}`}
-                    width={270}
                 />
             </CardBody>
         </Card>
